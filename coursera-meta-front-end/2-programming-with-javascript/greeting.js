95afaadd0592d3bf0df76e6f30c2ea6c1acd3a6c
ingle govindraj suresh <inglesuresh@cdacnoida.in>
2024-11-14 14:52:26 +0530

    css block design

---
+++ coursera-meta-front-end/2-programming-with-javascript/greeting.js
@@ -0,0 +1,10 @@
+export const informalGreeting = (person) => {
+    console.log(`Hello, ${person}`);
+}
+export const formalGreeting = (person) => {
+    console.log(`Good morning, ${person}`);
+}
+const greeting = () => {
+    console.log('Howdy !');
+}
+export default greeting;
